4dd723eacc3687f615c95f013bf76ebe790fd82b
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-03-19 16:07:44 -0700

    fixes

--- regex.py
+++ regex.py
@@ -1,14 +1,17 @@
 import re
 
 
-def heading(line: str):
-    return re.sub(r"=", r"#", line)
+def conversion(line: str):
+    temp = ""
+    # heading
+    temp = re.sub(r"=", r"#", line)
+    return temp
 
 
 def convert():
     with open("example.typ", "r", encoding="utf-8") as f:
         for i, line in enumerate(f, start=1):
-            converted_heading = heading(line.strip())
+            converted_heading = conversion(line.strip())
             print(i, line.strip())
             print(i, converted_heading)
             print("\n")
