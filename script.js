3ac2dc31de1c5af1d36c2c24f2702a92ec67de99
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-08-22 19:53:02 -0700

    basic scraping and html popup

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const html = document.documentElement.innerHTML;
+const shelf = [...document.querySelectorAll("span.h1Shelf")]
+  .find((span) => span.textContent.trim().startsWith("Want to Read"));
 
-console.log(html);
+const books = [...document.querySelectorAll("table#books tr")]
+  .map((row) => {
+    const cover = row.querySelector("td.field.cover div.value a");
+    const title = row.querySelector("td.field.title div.value a");
+    const author = row.querySelector("td.field.author div.value a");
+
+    return {
+      cover: cover?.querySelector("img")?.src ?? cover?.href ?? null,
+      title: title?.textContent.trim() ?? null,
+      author: author?.textContent.trim() ?? null,
+    };
+  })
+  .filter((book) => book.title);
+
+if (shelf && books) {
+  console.log("Want to Read");
+  console.log(books);
+
+}
