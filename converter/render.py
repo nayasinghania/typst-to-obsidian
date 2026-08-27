0fb3f1e062012852bc9657b6f5bcc542d4ce917d
Naya Singhania <74111380+nayasinghania@users.noreply.github.com>
2026-08-26 17:41:57 -0700

    bold text conversion

--- converter/render.py
+++ converter/render.py
@@ -16,9 +16,22 @@ def render_heading(node: JsonNode) -> str:
   return f'{"#" * level} {content}'
 
 
+def render_strong(node: JsonNode) -> str:
+  content = ''
+  for child in node.get('children', []):
+    if child['kind'] == 'Star':
+      continue
+    elif child['kind'] == 'Markup':
+      content = render(child)
+  return f'{"**"}{content}{"**"}'
+
+
 def render(node: JsonNode) -> str:
   if node['kind'] == 'Heading':
     return render_heading(node)
+
+  if node['kind'] == 'Strong':
+    return render_strong(node)
 
   if 'children' in node:
     return ''.join(render(child) for child in node['children'])
